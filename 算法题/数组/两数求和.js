8bd447403979bd17782b27382927890123976887
Grizz <panan521@qq.com>
2020-10-10 18:06:30 +0800

    两数求和

--- 算法题/数组/两数求和.js
+++ 算法题/数组/两数求和.js
@@ -6,18 +6,15 @@
 因为 nums[0] + nums[1] = 2 + 7 = 9 所以返回 [0, 1]
  */
 const twoSum = function (nums, target) {
-    // 这里我用对象来模拟 map 的能力
     const diffs = {}
-    // 缓存数组长度
     const len = nums.length
-    // 遍历数组
     for (let i = 0; i < len; i++) {
-        // 判断当前值对应的 target 差值是否存在（是否已遍历过）
         if (diffs[target - nums[i]] !== undefined) {
-            // 若有对应差值，那么答案get！
             return [diffs[target - nums[i]], i]
         }
-        // 若没有对应差值，则记录当前值
         diffs[nums[i]] = i
     }
 };
+//1.做和的题用差来做
+//2.活用对象的属性、
+//3.时间复杂度为n的二次方的，尝试降低时间复杂度

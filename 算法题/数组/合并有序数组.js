01a5fbd7e83da768d6b4a67a531b3b8fdfdb07cb
Grizz <panan521@qq.com>
2020-10-27 09:56:21 +0800

    三数求和问题，对撞指针解决方案

---
+++ 算法题/数组/合并有序数组.js
@@ -0,0 +1,47 @@
+/*
+真题描述：给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ 说明: 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）
+来保存 nums2 中的元素。 
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6], n = 3
+输出: [1,2,2,3,5,6]
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+    // 初始化两个指针的指向，初始化 nums1 尾部索引k
+    let i = m - 1, j = n - 1, k = m + n - 1
+    // 当两个数组都没遍历完时，指针同步移动
+    while(i >= 0 && j >= 0) {
+        // 取较大的值，从末尾往前填补
+        if(nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i] 
+            i-- 
+            k--
+        } else {
+            nums1[k] = nums2[j] 
+            j-- 
+            k--
+        }
+    }
+    
+    // nums2 留下的情况，特殊处理一下 
+    while(j>=0) {
+        nums1[k] = nums2[j]  
+        k-- 
+        j--
+    }
+};
+//双指针解题思路一般是针对有序数组，用空间换时间
+/**
+另外一个双指针解题的题目
+三数求和
+真题描述：给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+注意：答案中不可以包含重复的三元组。
+示例： 给定数组 nums = [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+ */

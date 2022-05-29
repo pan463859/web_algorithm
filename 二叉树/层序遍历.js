c8c0ed1c8607520d5a3597cf9c86370c3e753f45
潘小安 <panan521@qq.com>
2022-05-29 10:25:19 +0800

    更新一次层序遍历代码

--- 二叉树/层序遍历.js
+++ 二叉树/层序遍历.js
@@ -10,45 +10,67 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-let obj={
-    val:1,
-    left:{
-        val:2,
-        left:{
-            val:4
+let obj = {
+    val: 1,
+    left: {
+        val: 2,
+        left: {
+            val: 4
         },
-        right:{
-            val:5
+        right: {
+            val: 5
         }
     },
-    right:{
-        val:3,
-        left:{
-            val:6
+    right: {
+        val: 3,
+        left: {
+            val: 6
         },
-        right:{
-            val:7
+        right: {
+            val: 7
         }
     }
 }
- var levelOrder = function(root) {
-    let result=[]
-    let queue=[]
-    if(!root){
+var levelOrder = function (root) {
+    let result = []
+    let queue = []
+    if (!root) {
         return []
     }
     queue.push(root)
-    while(queue.length>0){
+    while (queue.length > 0) {
         result.push([])
-        let queuelength=queue.length
+        let queuelength = queue.length
         console.log(queuelength)
-        for(let i=0;i<queuelength;i++){
-            result[result.length-1].push(queue[i].val)
-            !!queue[i].left&&queue.push(queue[i].left)
-            !!queue[i].right&&queue.push(queue[i].right)
+        for (let i = 0; i < queuelength; i++) {
+            result[result.length - 1].push(queue[i].val)
+            !!queue[i].left && queue.push(queue[i].left)
+            !!queue[i].right && queue.push(queue[i].right)
         }
-        queue.splice(0,queuelength)
+        queue.splice(0, queuelength)
     }
     return result
 };
 levelOrder(obj)
+
+//2022 年 5.29日
+var levelOrder = function (root) {
+    let queue = [];
+    let res = []
+    if (root == null) {
+        return res
+    }
+    queue.push(root)
+    while (queue.length > 0) {
+        const len = queue.length
+        const curlevel = []
+        for (let i = 0; i < len; i++) {
+            const curnode = queue.shift()
+            curlevel.push(curnode.val)
+            curnode.left && queue.push(curnode.left);
+            curnode.right && queue.push(curnode.right)
+        }
+        res.push(curlevel)
+    }
+    return res
+};

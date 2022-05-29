54356210f99d33f260deccea22f6fb815451901b
潘小安 <panan521@qq.com>
2022-05-29 23:26:25 +0800

    修改一下代码

--- 二叉树/104.二叉树最大深度.js
+++ 二叉树/104.二叉树最大深度.js
@@ -11,23 +11,19 @@ var maxDepth = function (root) {
 };
 
 ////层序遍历思路的 bfs 法
-var maxDepth = function (root) {
+var maxDepth = function(root) {
     if (root === null) return 0;
     let queue = [root];
     let deepth = 0;
-    let res = []
     while (queue.length) {
         let n = queue.length;
         deepth++;
-        for (let i = 0; i < n; i++) {
+        for (let i=0; i<n; i++) {
             let node = queue.shift();
             // 如果左右节点都是null，则该节点深度最小
-            if (node.left === null && node.right === null) {
-                res.push(deepth)
-            }
             node.left && queue.push(node.left);;
-            node.right && queue.push(node.right);
+            node.right && queue.push (node.right);
         }
     }
-    return res[res.length - 1];
+    return deepth;
 };

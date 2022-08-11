d4098bd332bf88b0246fae1d9686378f8869bbb4
潘小安 <panan521@qq.com>
2022-08-12 01:56:45 +0800

    Update 103.找硬币.js
     feat：重写一次最小硬币咯

--- 动态规划/103.找硬币.js
+++ 动态规划/103.找硬币.js
@@ -18,31 +18,54 @@
 f(amout) = Math.min(f(amout-c1)+1,f(amout-c2)+1,f(amout-c3)+1......f(amout-cn)+1)
 这套对应关系，就是本题的状态转移方程。
  */
-const coinChange = function (coins, amount) {
-    // 用于保存每个目标总额对应的最小硬币个数
-    const c = []
-    // 提前定义已知情况
-    c[0] = 0
-    // 遍历 [1, amount] 这个区间的硬币总额
-    for (let currentAmount = 1; currentAmount <= amount; currentAmount++) {
-        // 求的是最小值，因此我们预设为无穷大，确保它一定会被更小的数更新
-        c[currentAmount] = Infinity
-        // 循环遍历每个可用硬币的面额
-        for (let j = 0; j < coins.length; j++) {
-            // 若硬币面额小于目标总额，则问题成立
-            if (currentAmount - coins[j] >= 0) {
-                // 状态转移方程
-                //currentAmount-coins[j],减去当前硬币面值后剩下的钱，
-                c[currentAmount] = Math.min(c[currentAmount], c[currentAmount - coins[j]] + 1)
-            }
-        }
-    }
-    // 若目标总额对应的解为无穷大，则意味着没有一个符合条件的硬币总数来更新它，本题无解，返回-1
-    if (c[amount] === Infinity) {
-        return -1
-    }
-    // 若有解，直接返回解的内容
-    return c[amount]
-};
+// const coinChange = function (coins, amount) {
+//     // 用于保存每个目标总额对应的最小硬币个数
+//     const c = []
+//     // 提前定义已知情况
+//     c[0] = 0
+//     // 遍历 [1, amount] 这个区间的硬币总额
+//     for (let currentAmount = 1; currentAmount <= amount; currentAmount++) {
+//         // 求的是最小值，因此我们预设为无穷大，确保它一定会被更小的数更新
+//         c[currentAmount] = Infinity
+//         // 循环遍历每个可用硬币的面额
+//         for (let j = 0; j < coins.length; j++) {
+//             // 若硬币面额小于目标总额，则问题成立
+//             if (currentAmount - coins[j] >= 0) {
+//                 // 状态转移方程
+//                 //currentAmount-coins[j],减去当前硬币面值后剩下的钱，
+//                 c[currentAmount] = Math.min(c[currentAmount], c[currentAmount - coins[j]] + 1)
+//             }
+//         }
+//     }
+//     // 若目标总额对应的解为无穷大，则意味着没有一个符合条件的硬币总数来更新它，本题无解，返回-1
+//     if (c[amount] === Infinity) {
+//         return -1
+//     }
+//     // 若有解，直接返回解的内容
+//     return c[amount]
+// };
 
-coinChange([1, 2, 5], 15)
+function coinChange(coins, amout) {
+  const dp = [];
+  // 初始值
+  dp[0] = 0;
+  coins.sort((a, b) => {
+    return a - b;
+  });
+  for (let curamount = 1; curamount <= amout; curamount++) {
+    dp[curamount] = Infinity;
+    for (let curcoin = 1; curcoin < coins.length; curcoin++) {
+      if (curamount - coins[curcoin] >= 0) {
+        // 转移方程
+        dp[curamount] = Math.min(
+          dp[curamount],
+          dp[curamount - coins[curcoin]] + 1
+        );
+      } else {
+        break;
+      }
+    }
+  }
+  return dp[amout] === Infinity ? -1 : dp[amout];
+}
+coinChange([1, 2, 5], 15);

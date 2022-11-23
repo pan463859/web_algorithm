3f757badeef0078f0bab9669e783db5fdcb2e992
潘小安 <panan521@qq.com>
2022-11-23 21:52:38 +0800

    solved:贪心算法合并区间

--- 贪心/56.合并区间.js
+++ 贪心/56.合并区间.js
@@ -9,8 +9,25 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
+var merge = function (intervals) {
+    const sortedIntervals = intervals.sort(function (a, b) {
+        return a[0] - b[0]
+    })
+    const res = []
+    let preRange = sortedIntervals[0]
+    for (let i = 1; i < sortedIntervals.length; i++) {
+        const nextRange = sortedIntervals[i];
+        // 下一个区间左侧在上一个区间内
+        if (nextRange[0] > preRange[1]) {
+            res.push(preRange)
+            preRange = nextRange
 
+        } else {
+            preRange = [preRange[0], Math.max(preRange[1], nextRange[1])]
+        }
+    }
+    res.push(preRange)
+    return res
 };
 // @lc code=end
 
